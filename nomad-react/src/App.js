dca11b9ee4b1fd3af84229a472f32817cb280662
JeonSuna <jsa020910@gmail.com>
2024-11-11 22:33:36 +0900

    useEffect를 활용한 byFun -cleanUp

--- nomad-react/src/App.js
+++ nomad-react/src/App.js
@@ -1,42 +1,29 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
-  const [keyword, setKeyword] = useState('')
-
-  const [counter, setValue] = useState(0)
-
-  const onClick = () => setValue((prev) => prev + 1)
-
-  const onChange = (event) => {
-    setKeyword(event.target.value)
+function Hello() {
+  function effectFun() {
+    console.log('created :)')
+    return destroyedFun
   }
-
-
-  useEffect(() => {
-    console.log('I run only once')
-  }, [])
-
-  useEffect(() => {
-    console.log('I run "keyword" change')
-  }, [keyword])
-
-  useEffect(() => {
-    console.log('I run "counter" change')
-  }, [counter])
-
-  useEffect(() => {
-    console.log('I run "keyword & counter" change')
-  }, [counter, keyword])
-
-
-  return (
-    <div>
-      <input value={keyword} onChange={onChange} type='text' placeholder="Search here..." ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
-
+  function destroyedFun() {
+    console.log('destroyed :(')
+  }
+  useEffect(effectFun, [])
+  return (<h1>
+    Hello
+  </h1>)
 }
 
+function App() {
+  const [showing, setShowing] = useState(false)
+  function onClick() {
+    return setShowing((prev) => !prev)
+  }
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'hide' : 'Show'}</button>
+    </div>
+  )
+}
 export default App;
