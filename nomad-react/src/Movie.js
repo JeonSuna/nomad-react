244b7346ec6805be35f2f9fcbd6d76e4bc9b8537
JeonSuna <jsa020910@gmail.com>
2024-11-17 18:09:55 +0900

    react-router

--- nomad-react/src/Movie.js
+++ nomad-react/src/Movie.js
@@ -1,3 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
 export default function Movie() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
